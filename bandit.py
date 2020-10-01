1a87ae0d78928f9b73d8d3857d1fd4ee0a7ccb85
Carsten Frommhold <carsten.frommhold@datadrivers.de>
2020-10-01 15:45:38 +0200

    simplify playing and play epsilon greedy

--- bandit.py
+++ bandit.py
@@ -1,6 +1,8 @@
-from random import normalvariate
+from random import normalvariate, random, randint
 from collections import namedtuple
 import numpy as np
+
+# Lets write a Bandit Agent Setup from Scratch
 
 Bandit_Expectation = namedtuple("Bandit_Expectation", ["No_explored", "Expected_mean"])
 
@@ -16,72 +18,73 @@ class Bandit():
     def play(self):
         return normalvariate(self.mean, 1)
 
+class Agent():
 
-def explore(bandit_expectations, no_steps, explicit_bandit=None):
+    def __init__(self, list_of_bandits, T):
+        self.list_of_bandits = list_of_bandits
+        self.no_of_bandits = len(list_of_bandits)
+        self.sum_won = 0
+        self.bandit_expectations = [Bandit_Expectation(0,0) for _ in self.list_of_bandits]
+        self.expected_to_be_highest = 0
+        self.T = T
 
-    """ Exploration Step
-    """
+    def play_bandit(self, bandit_no):
 
-    if explicit_bandit is None:
+        # Play once
+        won = self.list_of_bandits[bandit_no].play()
 
-        for bandit_no, bandit in enumerate(BANDITS, 0):
+        # Update wins
+        self.sum_won += won
 
-            expected_mean = bandit_expectations[bandit_no].Expected_mean
-            no_explored = bandit_expectations[bandit_no].No_explored
+        # Update expectation of bandit
+        expected_mean = self.bandit_expectations[bandit_no].Expected_mean
+        no_explored = self.bandit_expectations[bandit_no].No_explored
 
-            sum = 0
-            for trial in range(0, no_steps):
-                sum = sum + bandit.play()
-
-            # Update Bandit Expectation
-            bandit_expectations[bandit_no] = Bandit_Expectation(
-                Expected_mean = (expected_mean * no_explored + sum) / (no_explored + no_steps),
-                No_explored =  no_explored + no_steps
-            )
-
-        return bandit_expectations
-
-    else:
-
-        expected_mean = bandit_expectations[explicit_bandit].Expected_mean
-        no_explored = bandit_expectations[explicit_bandit].No_explored
-
-        sum = 0
-        for trial in range(0, no_steps):
-            sum = sum + BANDITS[explicit_bandit].play()
-
-        # Update Bandit Expectation
-        bandit_expectations[explicit_bandit] = Bandit_Expectation(
-            Expected_mean=(expected_mean * no_explored + sum) / (no_explored + no_steps),
-            No_explored=no_explored + no_steps
+        self.bandit_expectations[bandit_no] = Bandit_Expectation(
+            Expected_mean=(expected_mean * no_explored + won) / (no_explored + 1),
+            No_explored=no_explored + 1
         )
 
-        return bandit_expectations
+        # Update expected highest bandit
+        self.expected_to_be_highest = argmax(
+            [bandit_expectation.Expected_mean for bandit_expectation in self.bandit_expectations])
 
+    def play_epsilon_greedy(self, no_of_steps_first, epsilon):
+        """ Do Exploration Step first and then play random bandit with probability epsilon
+        """
+
+        # First step exploration
+        for step in range(0,no_of_steps_first):
+
+            for bandit_no in range(0, self.no_of_bandits):
+                self.play_bandit(bandit_no)
+
+        for _ in range(0,self.T-no_of_steps_first*self.no_of_bandits):
+
+            # Flip a coin
+            r = random()
+
+            # Explore or exploit
+            if r <= epsilon:
+                self.play_bandit(randint(0, self.no_of_bandits-1))
+            else:
+                self.play_bandit(bandit_no=self.expected_to_be_highest)
 
 
 
 ########### SIMULATION #############
 
-BANDITS = [Bandit(mean=bandit_mean) for bandit_mean in np.arange(0, 2.1, 0.2)]
-T = 1000
-n_bandits = len(BANDITS)
-trials_per_bandit = 2
-T_rest = T - trials_per_bandit*len(BANDITS)
+BANDITS = [Bandit(mean=bandit_mean) for bandit_mean in np.arange(0.2, 2.1, 0.2)]
+Agent01 = Agent(list_of_bandits=BANDITS, T=1000)
+
+Agent01.play_bandit(1)
+Agent01.play_epsilon_greedy(no_of_steps_first=2, epsilon=0.01)
 
 
-q = [Bandit_Expectation(0,0) for _ in BANDITS]
+print(f"We expect no {Agent01.expected_to_be_highest} to be the best bandit. \n")
+print("Summary: \n")
+for bandit_no, bandit_expectation in enumerate(Agent01.bandit_expectations, 1):
 
-
-q = explore(bandit_expectations=q, no_steps=10)
-q = explore(bandit_expectations=q, no_steps=2, explicit_bandit=1)
-
-print(q)
-final_choice = argmax([bandit_expectation.Expected_mean for bandit_expectation in q])
-print(final_choice)
-
-
-
-
-
-
+    print(f"Bandit No {bandit_no}:")
+    print(f"Expected Mean: {bandit_expectation.Expected_mean}")
+    print(f"Number played: {bandit_expectation.No_explored} \n")
